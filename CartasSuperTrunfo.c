d4a484dfa77de30c2136934f0355dc211a4d208f
Fit <docsfurgvini@gmail.com>
2025-09-10 05:38:39 +0000

    Atualização da comparação entre cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -33,14 +33,19 @@ char estado1;
 char codigo_carta1[3];
 char nome_cidade1[30];
 int pontos_tur1, populacao1;
-float area1, pib1, densidade1, pib_percapita1 = 0;
+float area1, pib1, densidade1, pib_percapita1 = 0, superpoder1;
+
 
 //Segunda carta
 char estado2;
 char codigo_carta2[3];
 char nome_cidade2[30];
 int pontos_tur2, populacao2;
-float area2, pib2, densidade2, pib_percapita2 = 0;
+float area2, pib2, densidade2, pib_percapita2 = 0, superpoder2;
+
+//Calculo dos resultados
+int resultadoArea, resultadoPib, resultadoPontos,
+resultadoPopulacao, resultadoSuperpoder, resultadoPibPercapita, resultadoDensidade;
 
 
 //Início da primeira carta
@@ -75,12 +80,13 @@ scanf("%d", &pontos_tur1);
 
 printf("\nCarta criada com sucesso!\n");
 
-//Calcúlo dos outros desafios
+//Calcúlo do desafio (Densidade populacional, PIB per capita e Super Poder)
 
-densidade1 = (float) populacao1 / area1;
+densidade1 = (float)populacao1 / area1;
 pib_percapita1 = (pib1 * 1000000000.0f) / (float)populacao1;
+superpoder1 = (float)populacao1 + area1 + pib1 + (float)pontos_tur1 + (1.0f / densidade1) + pib_percapita1;
 
-//Esqueleto printf da carta 1
+//Esqueleto printf da carta1
 
 printf("\nCarta 1:");
 printf("\nEstado: %s\n", nome_estado(estado1));
@@ -92,7 +98,7 @@ printf("PIB: %.2f bilhões de reais\n", pib1);
 printf("Número de Pontos Turísticos: %d\n", pontos_tur1);
 printf("Densidade populacional: %.2f hab/km²\n", densidade1);
 printf("PIB per capita: %.2f reais\n", pib_percapita1);
-
+printf("Super Poder: %.2f\n", superpoder1);
 
 //Início da segunda carta----------------------------------------------------------------------
 
@@ -124,12 +130,13 @@ scanf("%d", &pontos_tur2);
 //Mensagem de retorno dos dados montados na carta
 printf("\nCarta criada com sucesso!\n");
 
-//Calcúlo dos outros desafios
+//Calcúlo do desafio (Densidade populacional, PIB per capita e Super Poder)
 
-densidade2 = (float) populacao2 / area2;
+densidade2 = (float)populacao2 / area2;
 pib_percapita2 = (pib2 * 1000000000.0f) / (float)populacao2;
+superpoder2 = (float)populacao2 + area2 + pib2 + (float)pontos_tur2 + (1.0f / densidade2) + pib_percapita2;
 
-//Esqueleto printf da carta 2
+//Esqueleto printf da carta2
 
 printf("\nCarta 2:\n");
 printf("Estado: %s\n", nome_estado(estado2));
@@ -139,9 +146,29 @@ printf("População: %d\n", populacao2);
 printf("Área: %.2f km²\n", area2);
 printf("PIB: %.2f bilhões de reais\n", pib2);
 printf("Número de Pontos Turísticos: %d\n", pontos_tur2);
-printf("Densidade populacional: %.2f hab/km²\n", (float) densidade2);
-printf("PIB per capita: %.2f reais\n", (float) pib_percapita2);
+printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+printf("PIB per capita: %.2f reais\n", pib_percapita2);
+printf("Super Poder: %.2f\n", superpoder2);
 
+//Comparação das cartas
+
+resultadoPopulacao = populacao1 > populacao2;
+resultadoArea = area1 > area2;
+resultadoPib = pib1 > pib2;
+resultadoPontos = pontos_tur1 > pontos_tur2;
+resultadoDensidade = densidade1 < densidade2;
+resultadoPibPercapita = pib_percapita1 > pib_percapita2;
+resultadoSuperpoder = superpoder1 > superpoder2;
+
+printf("\nComparação entre as cartas:\n");
+
+printf("População: %s (%d)\n", resultadoPopulacao ? "Carta 1 venceu" : "Carta 2 venceu", resultadoPopulacao);
+printf("Área: %s (%d)\n", resultadoArea ? "Carta 1 venceu" : "Carta 2 venceu", resultadoArea);
+printf("PIB: %s (%d)\n", resultadoPib ? "Carta 1 venceu" : "Carta 2 venceu", resultadoPib);
+printf("Pontos Turísticos: %s (%d)\n", resultadoPontos ? "Carta 1 venceu" : "Carta 2 venceu", resultadoPontos);
+printf("Densidade: %s (%d)\n", resultadoDensidade ? "Carta 1 venceu" : "Carta 2 venceu", resultadoDensidade);
+printf("PIB per capita: %s (%d)\n", resultadoPibPercapita ? "Carta 1 venceu" : "Carta 2 venceu", resultadoPibPercapita);
+printf("Super Poder: %s (%d)\n", resultadoSuperpoder ? "Carta 1 venceu" : "Carta 2 venceu", resultadoSuperpoder);
 
 return 0;
 
